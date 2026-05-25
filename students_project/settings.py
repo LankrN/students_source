fd47d64e219d75ddeff0fb942b442fecf1cb2c90
LankrN <stas.xstrike2008@gmail.com>
2026-05-25 15:48:31 +0600

    Fix2: Update ALLOWED_HOSTS for Vercel deployment

--- students_project/settings.py
+++ students_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_pexr1uyc2@crl2gq*d_022r1#1c=fu(cy3s#70!!a(jkj-i0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
